ece6a0bcc30e2bda52dcbee3eaca9802d33bce3b
shuyicao <63182166+shuyicao@users.noreply.github.com>
2020-11-03 22:57:32 +1100

    Update case study.py

--- case study.py
+++ case study.py
@@ -1,38 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[32]:
-
-
 import pandas as pd
 import collections
 import numpy as np
 
 
-# In[34]:
-
-
 #read the file
-case = pd.read_csv('/Users/admin/Desktop/annotated.csv', encoding='unicode_escape')
-case
-
-
-# In[37]:
-
+case = pd.read_csv('annotated.csv', encoding='unicode_escape')
 
 case[['emo','prev_emo','next_emo','swda','prev_swda','next_swda']].describe()
 
-
-# In[40]:
-
-
-
 case['swda'].value_counts()
 
-
-# In[27]:
-
-
+#define function to return DA requency for emotion
 def frequency(emo_feature, da_feature, emo):
     print('the DA frequency for ' + emo + ' is ' )
     df = list(case.loc[case[emo_feature]==emo, da_feature])
@@ -44,9 +25,6 @@ def frequency(emo_feature, da_feature, emo):
 
 # ## current emo vs. current da
 
-# In[29]:
-
-
 print(frequency('emo','swda', 'anger'))
 print(frequency('emo','swda', 'disgust'))
 print(frequency('emo','swda', 'fear'))
@@ -56,9 +34,6 @@ print(frequency('emo','swda', 'surprise'))
 
 
 # ## previous emo vs. current da
-
-# In[30]:
-
 
 print(frequency('prev_emo','swda', 'anger'))
 print(frequency('prev_emo','swda', 'disgust'))
@@ -70,9 +45,6 @@ print(frequency('prev_emo','swda', 'surprise'))
 
 # ## current emo vs. previous da
 
-# In[41]:
-
-
 print(frequency('emo','prev_swda', 'anger'))
 print(frequency('emo','prev_swda', 'disgust'))
 print(frequency('emo','prev_swda', 'fear'))
@@ -83,45 +55,11 @@ print(frequency('emo','prev_swda', 'surprise'))
 
 # ## current emo vs. next da
 
-# In[31]:
-
-
 print(frequency('emo','next_swda', 'anger'))
 print(frequency('emo','next_swda', 'disgust'))
 print(frequency('emo','next_swda', 'fear'))
 print(frequency('emo','next_swda', 'happiness'))
 print(frequency('emo','next_swda', 'sadness'))
 print(frequency('emo','next_swda', 'surprise'))
-
-
-# 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 
 
